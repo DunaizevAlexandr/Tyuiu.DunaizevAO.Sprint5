7f8c55c9b0fdc8382e66c15a729c12990a28771e
Sasha Dunaizev <jenderprolite@gmail.com>
2025-11-25 20:08:35 +0500

    Task1

---
+++ Tyuiu.DunaizevAO.Sprint5.Task1.V12.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.DunaizevAO.Sprint5.Task1.V12.Lib
+{
+    public class DataService : ISprint5Task1V12
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+            int count = stopValue - startValue + 1;
+            string[] lines = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                int x = startValue + i;
+                double denominator = Math.Sin(x) - 2;
+                double value;
+
+                if (denominator == 0)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = (5 * x + 2.5) / denominator;
+                    value = Math.Round(value, 2);
+                }
+
+                lines[i] = $"{value:F2}".Replace(',', '.');
+            }
+
+            File.WriteAllLines(path, lines);
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint5.Task1.V12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Tyuiu.DunaizevAO.Sprint5.Task1.V12.Lib;
+
+int start = -5;
+int stop = 5;
+
+DataService ds = new DataService();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("start = " + start);
+Console.WriteLine("stop = " + stop);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+string res = ds.SaveToFileTextData(start,stop);
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
