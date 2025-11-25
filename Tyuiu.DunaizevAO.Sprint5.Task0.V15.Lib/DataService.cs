14a587d3fc6546c692756a479875ce80afc61de7
Sasha Dunaizev <jenderprolite@gmail.com>
2025-11-25 18:47:55 +0500

    task0

--- Tyuiu.DunaizevAO.Sprint5.Task0.V15.Lib/DataService.cs
+++ Tyuiu.DunaizevAO.Sprint5.Task0.V15.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.DunaizevAO.Sprint5.Task0.V15.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double y = (x) / (x * x * x + 2);
+            double y = (x) / (Math.Pow(x, 3) + 2);
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
             return path;
