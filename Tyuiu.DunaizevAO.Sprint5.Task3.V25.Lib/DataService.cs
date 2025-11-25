c849a31184b83d855b767c1b3acc06ac951b1444
Sasha Dunaizev <jenderprolite@gmail.com>
2025-11-25 22:35:48 +0500

    Task3

---
+++ Tyuiu.DunaizevAO.Sprint5.Task3.V25.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.DunaizevAO.Sprint5.Task3.V25.Lib
+{
+    public class DataService : ISprint5Task3V25
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.bin");
+            double y = (3 * Math.Pow(x, 4) + 1) / (3 * Math.Pow(x, 3));
+            y = Math.Round(y);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint5.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.DunaizevAO.Sprint5.Task3.V25.Lib;
+
+namespace Tyuiu.DunaizevAO.Sprint5.Task3.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\pixel\source\repos\Tyuiu.DunaizevAO.Sprint5\Tyuiu.DunaizevAO.Sprint5.Task3.V25\bin\Debug\net8.0\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
