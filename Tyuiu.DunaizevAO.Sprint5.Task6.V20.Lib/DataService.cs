e29e33996d917c57639889bf1920a0ad6740ef47
Sasha Dunaizev <jenderprolite@gmail.com>
2025-11-27 18:26:00 +0500

    Task6

--- Tyuiu.DunaizevAO.Sprint5.Task6.V20.Lib/DataService.cs
+++ Tyuiu.DunaizevAO.Sprint5.Task6.V20.Lib/DataService.cs
@@ -7,6 +7,7 @@ namespace Tyuiu.DunaizevAO.Sprint5.Task6.V20.Lib
         public int LoadFromDataFile(string path)
         {
             string path1 = File.ReadAllText(path);
+            path1 = path1.Replace('.', ' ');
             string[] str = path1.Split(' ');
             int res = 0;
             for (int i = 0; i < str.Length; i++)
