57e1d90b0058d3e8078b4b92a4473fdc71c92a21
Sasha Dunaizev <jenderprolite@gmail.com>
2025-11-27 18:24:48 +0500

    Task6

---
+++ Tyuiu.DunaizevAO.Sprint5.Task6.V20.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.DunaizevAO.Sprint5.Task6.V20.Lib
+{
+    public class DataService : ISprint5Task6V20
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string path1 = File.ReadAllText(path);
+            string[] str = path1.Split(' ');
+            int res = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length == 6)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint5.Task6.V20/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;
+using Tyuiu.DunaizevAO.Sprint5.Task6.V20.Lib;
+
+string path = Path.Combine("C:", "DataSprint5", "InPutDataFileTask5V8.txt");
+
+DataService ds = new DataService();
+Console.WriteLine(ds.LoadFromDataFile(path));
